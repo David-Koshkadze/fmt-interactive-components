c94f7e79c1977d10ba0197c5986d606afba61fe0
David-Koshkadze <rootUS3rr@pm.me>
2023-04-18 21:16:07 +0400

    add styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 import Card from "./Card";
-import styled from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+  :root {
+    --Moderate-blue: hsl(238, 40%, 52%);
+    --Soft-Red: hsl(358, 79%, 66%);
+    --Light-grayish-blue: hsl(239, 57%, 85%);
+    --Pale-red: hsl(357, 100%, 86%);
+    --Dark-blue: hsl(212, 24%, 26%);
+    --Grayish-Blue: hsl(211, 10%, 45%);
+    --Light-gray: hsl(223, 19%, 93%);
+    --Very-light-gray: hsl(228, 33%, 97%);
+    --White: hsl(0, 0%, 100%);
+  }
+`;
 
 const AppContainer = styled.div`
-  background: #eee;
+  background: var(--Light-gray);
   width: 100%;
   height: 100vh;
   display: flex;
@@ -12,10 +26,13 @@ const AppContainer = styled.div`
 
 function App() {
   return (
-    <AppContainer>
-      <Card />
-      <Card />
-    </AppContainer>
+    <>
+      <GlobalStyles />
+      <AppContainer>
+        <Card />
+        <Card />
+      </AppContainer>
+    </>
   );
 }
 
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,16 +1,69 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
-const Wrapper = styled.div`
+const CardWrapper = styled.div`
   background: #fff;
-  max-width: 400px;
-  border: 1px solid green;
-`
+  border-radius: 6px;
+  max-width: 700px;
+  display: flex;
+  gap: 1rem;
+  margin-bottom: 1rem;
+  padding: 1rem;
+`;
+
+const Voting = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0.4rem 0.7rem;
+  background-color: var(--Light-gray);
+  border-radius: 8px;
+  font-weight: 500;
+  gap: 0.4rem;
+  color: var(--Moderate-blue);
+`;
+
+const VoteButton = styled.div`
+  background: transparent;
+  color: var(--Light-grayish-blue);
+  font-weight: 700;
+  font-size: 17px;
+`;
+
+const FlexCol = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const FlexRow = styled.div`
+  display: flex;
+`;
 
 export default function Card() {
   return (
-    <Wrapper>
-      <h1>Hello World</h1> 
-    </Wrapper>
-  )
+    <CardWrapper>
+      <Voting>
+        <VoteButton>+</VoteButton>
+        <span>12</span>
+        <VoteButton>-</VoteButton>
+      </Voting>
+
+      <FlexCol>
+        <FlexRow justify="between">
+          <FlexRow>
+            <div>Avatar</div>
+            <span>1 month ago</span>
+          </FlexRow>
+          <div>Reply</div>
+        </FlexRow>
+
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque vel
+          deserunt temporibus eum veniam dolores accusamus modi aut similique
+          nobis, saepe nulla error culpa expedita consequuntur assumenda?
+        </p>
+      </FlexCol>
+    </CardWrapper>
+  );
 }
