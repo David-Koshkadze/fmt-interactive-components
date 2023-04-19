4b5045069c875473a3455f9aceff049ea8955ccf
David-Koshkadze <rootUS3rr@pm.me>
2023-04-19 10:04:12 +0400

    add some styles and assets

--- src/Card.jsx
+++ src/Card.jsx
@@ -16,12 +16,13 @@ const Voting = styled.div`
   flex-direction: column;
   justify-content: space-between;
   align-items: center;
-  padding: 0.4rem 0.7rem;
+  padding: 0.4rem 0.6rem;
   background-color: var(--Light-gray);
   border-radius: 8px;
   font-weight: 500;
-  gap: 0.4rem;
+  gap: 0.7rem;
   color: var(--Moderate-blue);
+  height: max-content;
 `;
 
 const VoteButton = styled.div`
@@ -29,6 +30,7 @@ const VoteButton = styled.div`
   color: var(--Light-grayish-blue);
   font-weight: 700;
   font-size: 17px;
+  cursor: pointer;
 `;
 
 const FlexCol = styled.div`
@@ -38,6 +40,24 @@ const FlexCol = styled.div`
 
 const FlexRow = styled.div`
   display: flex;
+  align-items: center;
+  justify-content: ${(props) => props.justify || ""};
+  gap: ${(props) => props.gap || "0"};
+`;
+
+const TextReply = styled.span`
+  font-weight: 600;
+  font-size: 1rem;
+  color: var(--Moderate-blue);
+`;
+
+const TextAvatar = styled(TextReply)`
+  color: var(--Dark-blue);
+`;
+
+const TextGray = styled.p`
+  color: var(--Grayish-Blue);
+  line-height: 1.4rem;
 `;
 
 export default function Card() {
@@ -50,19 +70,28 @@ export default function Card() {
       </Voting>
 
       <FlexCol>
-        <FlexRow justify="between">
-          <FlexRow>
-            <div>Avatar</div>
-            <span>1 month ago</span>
+        <FlexRow justify="space-between" style={{ marginBottom: "1rem" }}>
+          <FlexRow gap="1rem">
+            <FlexRow gap="1rem">
+              <img
+                src="/images/avatars/image-amyrobson.png"
+                style={{ height: "2rem", width: "2rem" }}
+              />
+              <TextAvatar>amyrobson</TextAvatar>
+            </FlexRow>
+            <TextGray>1 month ago</TextGray>
           </FlexRow>
-          <div>Reply</div>
+          <FlexRow gap="0.5rem">
+            <img src="/images/icon-reply.svg" />
+            <TextReply>Reply</TextReply>
+          </FlexRow>
         </FlexRow>
 
-        <p>
+        <TextGray>
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque vel
           deserunt temporibus eum veniam dolores accusamus modi aut similique
           nobis, saepe nulla error culpa expedita consequuntur assumenda?
-        </p>
+        </TextGray>
       </FlexCol>
     </CardWrapper>
   );
